a15278e76e3199bced38ca44c3cdd1ad7d03cd96
LeeSarduy <30799283+LeeSarduy@users.noreply.github.com>
2017-10-26 11:25:54 -0400

    Add files via upload

---
+++ Implementation/MSSAProjectV01/MSSAProjectV01/Admin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSAProjectV01
+{
+    class Admin
+    {
+        protected string email;
+        protected byte[] password;
+        protected string FirstName;
+        protected string LastName;
+        protected string phone;
+
+        protected void CreateTeam()
+        {
+            //To do
+        }
+
+        protected void DeleteTeam()
+        {
+            //To do
+        }
+
+        protected void UpdateStats()
+        {
+            //To do
+        }
+    }
+}
+ 
+    
