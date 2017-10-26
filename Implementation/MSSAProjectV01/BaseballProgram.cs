59239d569abbfc7c41d9054b99b56b7dda222918
Oriette <31009251+Oriette@users.noreply.github.com>
2017-10-26 10:17:01 -0400

    Add files via upload

---
+++ Implementation/MSSAProjectV01/BaseballProgram.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Baseball
+
+{
+
+    class Captain
+
+    {
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public FirstName { get; set; }
+
+        public LastName { get; set; }
+
+        public string phone { get; set; }
+
+        public Soldier(string email, string password, Weapon, Firstname =null, LastName = null, string phone)
+
+        {
+
+            this.Name = name;
+
+            this.password = password;
+
+            this.FirstName = FirstName;
+
+            this.LastName = LastName;
+
+            this.phone = phone;
+
+        }
+
+    }
+
+}
+
+        }
+    }
+}
